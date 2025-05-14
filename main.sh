e4195ec4872cbd79e4c3aec9cda1c2c42db3bd08
Jiyoon Pyo <jiyoonp0228@gmail.com>
2025-05-14 10:10:49 -0500

    update

--- main.sh
+++ main.sh
@@ -1,15 +1,72 @@
 # Remove original connection
 # git remote remove origin
 
-read -p "Enter fullname: " fullname
+# val NEWREPO=false
 
-echo $fullname
+# # Connect to new remote github
+# read -p "Connect to repo? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [nN] ]] || exit 1
+# if [ $confirm = "y" || $confirm = "Y" ]; then
+#     read -p "GitHub remote repo: " reponame
+#     git init
+#     git remote add origin $reponame
+#     git branch -M main
+#     git add --all
+#     git commit -m "Repository initialization"
+#     git push -u origin main
+#     echo "Current folder has been added to GitHub repository"
 
-read -p "Option: " foldop
+#     val NEWREPO=true
+# fi
 
-read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+# # Create venv
+# read -p "Create venv? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [nN] ]] || exit 1
+# if [ $confirm = "y" || $confirm = "Y" ]; then
+#     read -p "Python version: " pyver
+#     read -p "pyenv name: " envname
+#     python$pyver -m venv $envname
+#     source $envname/bin/activate
 
-echo $confirm
+#     # Add venv to gitignore
+#     read -p "Add to gitignore? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [nN] ]] || exit 1
+#     if [ $confirm = "y" || $confirm = "Y" ]; then
+#         touch .gitignore
+#         echo $envname/ >> .gitignore
+#     fi
+# fi
+
+# Install some packages
+
+# Selection option
+# read -p "Initialization option? (basic/class): " confirm && [[ $confirm == "basic" || $confirm == "class" ]] || exit 1
+
+# if [ $confirm == "basic" ]; then
+#     rm -rf with_class
+#     mv ./basic/* ./
+# elif [ $confirm == "class" ]; then
+#     rm -rf basic
+#     read -p "Project name: " name
+#     mv ./with_class ./$name
+# fi
+
+CURRENTPATH="$(pwd)"/with_class
+echo $CURRENTPATH
+
+cd $CURRENTPATH
+sed -i "5s/.*/from sample import data/" $filename
+# sed -i -e $envname"/" .gitignore
+
+# echo pyver
+
+
+# echo $fullname
+
+# read -p "Option: " foldop
+
+
+# Double-checking information
+# read -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+
+# echo $confirm
 
 
 # User input
@@ -35,9 +92,14 @@ echo $confirm
 
 # mkdir
 
-# # git init
-# # git remote add origin ""
-# # git branch -M main
-# # git add --all
-# # git commit -m "branch initialization"
-# # git push -u origin main
+# Create new branch
+# if [ $NEWREPO = true ]; then
+#     read -p "Create new branch? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [nN] ]] || exit 1
+#     if [ $confirm = "y" || $confirm = "Y" ]; then
+#         read -p "GitHub branch name: " branchname
+#         git push --set-upstream origin $branchname
+#     fi
+# else
+#     git add --all
+#     git commit -m "Initialization"
+# fi
